6ba864ca2597af4dab5519450479171d378b7a67
PeterPirog <peterpirogtf@gmail.com>
2020-01-14 22:21:48 +0100

    Added Examples

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 print('Test')
 import humidity as hm
 
+
+#Example convertsion Kalvins to Celsius Degree
 print(hm.hum.K2C(290))
 
-print('test')
+#Example convertsion Celsius Degree to Kalvins
+print(hm.hum.KC2K(20))
