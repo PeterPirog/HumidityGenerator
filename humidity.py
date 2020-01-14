d4d1726014e98be4957f65ba5ed5fea86c6fcbc6
peterpirogtf <peterpirogtf@gmail.com>
2020-01-14 15:08:02 +0100

    Temperature conversion added

--- humidity.py
+++ humidity.py
@@ -1,3 +1,9 @@
 import numpy as np
 
-
+class hum:
+    def K2C(TempK):
+        TempC=TempK-273.15
+        return TempC
+    def C2K(TempC):
+        TempK=TempC+273.15
+        return TempK
--- main.py
+++ main.py
@@ -1,1 +1,4 @@
 print('Test')
+import humidity as hm
+
+print(hm.hum.K2C(290))
