f5b101c390cf2218ec75b8251994bed4cad62a39
peterpirogtf <peterpirogtf@gmail.com>
2020-01-29 15:01:26 +0100

    Literature added

--- GPIB_test.py
+++ GPIB_test.py
@@ -1,7 +1,27 @@
 import pyvisa
-rm = pyvisa.ResourceManager()
-rm.list_resources()
+import time
 
-inst = rm.open_resource('ASRL7::INSTR')
-inst.write_termination = '\r'
-print(inst.query("?"))
+rm = pyvisa.ResourceManager()
+print(rm.list_resources())
+
+inst = rm.open_resource('GPIB0::16::INSTR')
+print(inst.query("*IDN?"))
+inst.write_ascii_values('SYST:PRES','')
+time.sleep(1)
+inst.write_ascii_values('FUNC "FRES"','')
+inst.write_ascii_values('FRES:RANG 100','')
+inst.write_ascii_values('TRIG:DELAY 0','')
+inst.query("*OPC?")
+
+inst.write_ascii_values('ROUT:CLOS (@2)', '')
+inst.query("*OPC?")
+time.sleep(1)
+print(inst.query('FETCH?'))
+
+
+for i in range(10):
+    command='ROUT:CLOS (@{})'.format(str(i+1))
+    inst.write_ascii_values(command,'')
+    time.sleep(1)
+    inst.query("*OPC?")
+    print(inst.query('FETCH?'))
