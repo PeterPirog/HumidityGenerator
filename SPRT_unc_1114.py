a59f43f68bd4bcadf505957554f36e26e2f6e993
peterpirogtf <peterpirogtf@gmail.com>
2020-03-04 15:13:37 +0100

    Literature added

--- SPRT_unc_1114.py
+++ SPRT_unc_1114.py
@@ -24,20 +24,24 @@ R0=ureal(x=2.5534637,u=0.0000041/2,label='R0')
 
 #print(Calculate_temp_from_W(1.079488))
 
+#Dane ze świadectwa
 t=[961.78,660.332,419.527,231.928]
-#t_U=[0.0052,0.0040,0.0017,0.0015]
-t_U=[0.0052,0.0040,0.0017]
+t_U=[0.0052,0.0040,0.0017,0.0015]
 Wt=[4.28556295,3.37543469,2.56855103,1.89259628]
 
+#Konwersja liczb do postaci ureal
 t=lists2ureal(t,t_U)
 Wt=lists2ureal(Wt)
 
+Wr=map(Calculate_Wr,t)
 
 
-print(t)
-print(Wt)
+print('t=',t)
+print('Wt=',Wt)
+print('Wr=',Wr)
 
-
+for item in map(Calculate_Wr,t):
+    print(item)
 
 path = r'C:\Users\Admin\PycharmProjects\HumidityGenerator\SPRT_nr_1114.xlsx'
 book=openpyxl.load_workbook(path)
